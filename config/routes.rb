86c5f530fe6e3c3bdfc4ad9e9d9cbd2e7173db29
Daniel Enrriquez <dan_enrriquez@outlook.com>
2023-06-06 16:28:41 -0600

    corrected routes and corrected tipo in pet.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  get 'adoption_requests/index'
-  get 'adoption_requests/new'
-  get 'adoption_requests/create'
-  get 'adoption_requests/destroy'
-  get 'pets/index'
-  get 'pets/create'
-  get 'pets/new'
-  get 'pets/show'
-  get 'pets/edit'
-  get 'pets/destroy'
-  get 'pets/update'
   devise_for :users
   root to: "pages#home"
   resources :pets do
