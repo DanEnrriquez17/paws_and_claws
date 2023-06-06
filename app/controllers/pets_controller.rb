63a1d34c0aa871b70a5faf649949d8c4c49a44d0
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-06-06 11:53:30 -0600

    created controllers for pets and adoption requests

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,22 @@
+class PetsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
