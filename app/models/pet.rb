31d927445201744510c9c50425078aaefe37dde7
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-06-08 16:19:13 -0600

    accepted current change to pet.rb

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,8 +6,5 @@ class Pet < ApplicationRecord
 
   validates :name, :species, :breed, :age, presence: true
   validates :species, inclusion: { in: CATEGORIES }
-<<<<<<< HEAD
   has_one_attached :photo
-=======
->>>>>>> master
 end
