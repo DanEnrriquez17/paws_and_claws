8c8783ae43bb3af627d9fb90d7549f43a7dd1784
Daniel Enrriquez <dan_enrriquez@outlook.com>
2023-06-08 18:02:18 -0600

    created a new view and solve the logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #do
+    #resources :adoption_requests, only: [:index]
+  #end
   root to: "pets#index"
   resources :pets do
-    resources :adoption_requests, except: [:edit, :show, :create]
+    resources :adoption_requests, except: [:edit, :show]
   end
-
-  post "pets/:pet_id/create_request/", to: "adoption_requests#create", as: "create_request"
+  get "my_pets_adoption_requests", to: "users#requests", as: "my_pets_requests"
   patch "pets/:pet_id/accept_request/:id", to: "adoption_requests#accept", as: "accept_request"
-
+#get "users/:id/adoption_requests/index", to: "users#adoption_requests_index", as: "users_req_made"
 
 end
